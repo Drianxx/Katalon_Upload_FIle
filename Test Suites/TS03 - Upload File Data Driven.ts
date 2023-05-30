fba74f0eea94024e7099b0ac41d10178dfa82c09
adria <adria@DESKTOP-2B5N6V0>
2023-05-30 15:52:59 +0700

    Add TS03 - Data Driven

---
+++ Test Suites/TS03 - Upload File Data Driven.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS03 - Upload File Data Driven</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2532ae5e-8fac-4ccd-8a97-bf2e510fa276</testSuiteGuid>
+   <testCaseLink>
+      <guid>6fab3157-8d3f-4c55-a8a5-2aef0ac937fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <iterationNameVariable>
+         <defaultValue>''</defaultValue>
+         <description></description>
+         <id>30afb2e6-2ad3-47e5-95ae-eede2d3251b8</id>
+         <masked>false</masked>
+         <name>path</name>
+      </iterationNameVariable>
+      <testCaseId>Test Cases/TS03 - Upload File Data Driven</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1f9dbf8e-d568-4014-b654-ff9860abc77a</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DataPath</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>1f9dbf8e-d568-4014-b654-ff9860abc77a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Path</value>
+         <variableId>30afb2e6-2ad3-47e5-95ae-eede2d3251b8</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
